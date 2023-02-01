14a8b07b45cfb8f7610b9074cfa1e9edc5b5539a
Ogas <83815549+Unclotho@users.noreply.github.com>
2023-02-01 15:18:59 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+  <Ogas/>
+</template>
+
+<script>
+import Ogas from './components/Ogas.vue'
+export default {  
+  name: 'App',
+  components: {
+    Ogas
+  }
+}
+</script>
---
+++ src/components/Ogas.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Ogas</div>
+</template>
